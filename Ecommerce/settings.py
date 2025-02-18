d6a028ba475c405b919860399701ac61c5933c43
VG-1510 <vaibhavgohil305@gmail.com>
2025-02-18 22:26:00 +0530

    new commit

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -61,7 +61,7 @@ MIDDLEWARE = [
     
 ]
 # 'whitenoise.middleware.WhiteNoiseMiddleware',
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = 'Ecommerce.urls'
 
