bd8799f15ea835178311b90e00b1c090b142552b
VG-1510 <vaibhavgohil305@gmail.com>
2025-02-18 21:34:26 +0530

    new commit 8

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -57,8 +57,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', 
     
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = 'Ecommerce.urls'
 
