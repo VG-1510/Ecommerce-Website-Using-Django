e101f4563c7b03f3bd49a5ece3ea7bddce34d14f
VG-1510 <vaibhavgohil305@gmail.com>
2025-02-18 21:15:37 +0530

    new commit 2

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -34,7 +34,7 @@ EMAIL_HOST_USER = 'gohilvaibhav31@gmail.com'
 EMAIL_HOST_PASSWORD = 'nhpyipdvepxdfawg'
 DEFAULT_FROM_EMAIL = 'gohilvaibhav31@gmail.com'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['crud-janv.onrender.como']
 
 
 # Application definition
